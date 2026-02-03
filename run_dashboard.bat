d96cb4ca442b29a9944d35c6d7f7c9f082dd4342
3bdullah-0 <abdullahjbear19@gmail.com>
2026-02-04 00:57:49 +0300

    Update run_dashboard.bat

--- run_dashboard.bat
+++ run_dashboard.bat
@@ -1,7 +1,1 @@
-@echo off
-echo Starting 3BD Barber Shop System...
-echo ------------------------------------------
-echo Server getting ready...
-start "" "http://localhost:3000"
-npm start
-pause
+
